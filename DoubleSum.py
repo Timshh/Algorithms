084d80ef3ab2135baac176e37427a521bf0049da
Shirobokih Timofei Sergeevich <shirobokihtim@gmail.com>
2026-05-08 23:21:07 +0300

    New algorithms
    Bubble sort
    Double summ
    Selection sort

---
+++ DoubleSum.py
@@ -0,0 +1,15 @@
+﻿def DoubleSum(A, B):
+    C = []
+    shouldAdd = False
+    for i in range(len(A) - 1, -1, -1):
+        num = A[i] + B[i] + int(shouldAdd)
+        C.append(num%2)
+        shouldAdd = False
+        if num > 1:
+            shouldAdd = True
+    if shouldAdd:
+        C.append(1)
+    C.reverse()
+    return C
+
+print (DoubleSum([1, 0, 0, 1, 1, 0, 0, 1, 1], [1, 0, 0, 1, 1, 0, 0, 1, 1]))
