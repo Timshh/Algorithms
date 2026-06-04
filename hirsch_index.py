b4cb956b77ceb78a1ae9630f9b5714a7cc6ab7b8
Shirobokih Timofei Sergeevich <shirobokihtim@gmail.com>
2026-06-04 18:53:44 +0300

    Hirsch index and quicksort

---
+++ hirsch_index.py
@@ -0,0 +1,15 @@
+﻿def hirsch_id(a):
+    maximum = 0
+    h_id = 0
+    for i in a:
+        maximum = max(i, maximum)
+    for i in range(maximum, 0, -1):
+        count = 0
+        for j in a:
+            if j >= i: count += 1
+        if count >= i:
+            h_id = i
+            break
+    return h_id
+
+print(hirsch_id([3, 0, 6, 1, 5, 100, 50, 20, 10, 7, 8, 17, 197, 76, 31]))
---
+++ qsort.py
@@ -0,0 +1,20 @@
+﻿def qsort(a, left, right):
+    key = a[(left + right) // 2]
+    i = left
+    j = right
+    while i <= j:
+        while a[i] < key:
+            i += 1
+        while a[j] > key:
+            j -= 1
+        if i <= j:
+            a[i], a[j] = a[j], a[i]
+            i += 1
+            j -= 1
+    if left < j:
+        a = qsort(a, left, j)
+    if i < right:
+        a = qsort(a, i, right)
+    return a
+
+print(qsort([5, 1, 2, 4, 3], 0, 4))
